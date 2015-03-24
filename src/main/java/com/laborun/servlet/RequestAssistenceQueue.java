7d088b5ba69129f9427151292e01d9ce9e753b1a
Dina_I35_EWD <dina.abdelbadea2@gmail.com>
2015-03-24 15:27:20 -0700

    m

--- src/main/java/com/laborun/servlet/RequestAssistenceQueue.java
+++ src/main/java/com/laborun/servlet/RequestAssistenceQueue.java
@@ -5,31 +5,20 @@
  */
 package com.laborun.servlet;
 
-import com.laborun.controller.CourseImp;
-import com.laborun.controller.LabImp;
-import com.laborun.controller.LabInt;
 import com.laborun.controller.QueueImp;
-import com.laborun.controller.StaffImp;
-import com.laborun.entity.Course;
-import com.laborun.entity.Lab;
 import com.laborun.entity.QueueD;
-import com.laborun.entity.Staff;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author dina
  */
-public class NewLab extends HttpServlet {
+public class RequestAssistenceQueue extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -42,7 +31,7 @@ public class NewLab extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+       
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -57,15 +46,7 @@ public class NewLab extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-     
-       HttpSession session = request.getSession(true);
-       CourseImp CI = new CourseImp();
-       StaffImp SI = new StaffImp();
-      List<Course> c = CI.getCourses();
-      List<Staff> s = SI.getStaff();
-      session.setAttribute("cList",c);
-      session.setAttribute("sList",s);
-       response.sendRedirect("/LaboRun/admin/addLab.jsp");   
+        processRequest(request, response);
     }
 
     /**
@@ -79,41 +60,9 @@ public class NewLab extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Set Queues = new HashSet();
-        Lab lab = new Lab();
-        QueueD assistanceQ = new QueueD();
-        assistanceQ.setQueueType("ass");
-        QueueD delivryQ = new QueueD();
-        delivryQ.setQueueType("del");
-       
-       lab.setLabName(request.getParameter("LabName"));
-       String[] ss = request.getParameterValues("staffn");
-      
-       Set staffMembers = new HashSet();
-       for(String n1 : ss)
-      {
-          Staff s = new Staff();
-          s.setId(Integer.parseInt(n1));
-          staffMembers.add(s);
-      }
-       lab.setStaffs(staffMembers);
-       Course c = new Course();
-       c.setId(Integer.parseInt(request.getParameter("coursen")));
-       lab.setCourse(c);
-       LabInt LI = new LabImp();
-       
-        assistanceQ.setLab(lab);
-        delivryQ.setLab(lab);
-        QueueImp q = new QueueImp();
+        QueueImp QI = new QueueImp();
+        QueueD q = new QueueD();
         
-        Queues.add(delivryQ);
-        Queues.add(assistanceQ);
-        lab.setQueueDs(Queues);
-        LI.insertLab(lab);
-        q.requestQueue(delivryQ);
-        q.requestQueue(assistanceQ);
-       
-       
     }
 
     /**
